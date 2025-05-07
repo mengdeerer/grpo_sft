45dcdf17d422b1efad7e0772964d3ceb93dbaac1
mengdeerer <mengdeerer@gmail.com>
2025-05-07 20:59:00 +0800

    first commit

---
+++ open-r1/scripts/grpo.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=4,5,6,7 ACCELERATE_LOG_LEVEL=info \
+    accelerate launch --config_file recipes/accelerate_configs/zero2.yaml --num_processes 7 \
+    src/open_r1/grpo.py --config recipes/DeepSeek-R1-Distill-Qwen-1.5B/grpo/config_demo.yaml
