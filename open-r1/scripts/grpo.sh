f27a76408d5ceb4293835116727bb73ceea05b79
lab <mengdeerer@gmail.com>
2025-05-20 08:17:48 +0800

    改为16384,accu=4进一步训练

--- open-r1/scripts/grpo.sh
+++ open-r1/scripts/grpo.sh
@@ -14,3 +14,6 @@ CUDA_VISIBLE_DEVICES=1,2,4,5,6,7 ACCELERATE_LOG_LEVEL=info \
 CUDA_VISIBLE_DEVICES=1,2,3 ACCELERATE_LOG_LEVEL=info \
     accelerate launch --config_file recipes/accelerate_configs/zero3.yaml --num_processes 3 \
     src/open_r1/grpo.py --config recipes/DeepSeek-R1-Distill-Qwen-1.5B/grpo/config_demo.yaml > terminal.txt
+
+
+make evaluate MODEL=/mnt/xjl/checkpoints/sft-deepseek/DeepSeek-1.5B-GRPO-aime/checkpoint-877 TASK=aime24 PARALLEL=data NUM_GPUS=4
